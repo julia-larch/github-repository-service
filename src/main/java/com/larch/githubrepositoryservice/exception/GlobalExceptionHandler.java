7df90f6f231d2043ef6c23b1b968a9c767a5898b
Yuliya Larchanka <julia.larch7@gmail.com>
2026-01-05 23:11:21 +0100

    Add logging levels

--- src/main/java/com/larch/githubrepositoryservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/larch/githubrepositoryservice/exception/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.larch.githubrepositoryservice.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
+        log.warn("User not found error: {}", e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(
